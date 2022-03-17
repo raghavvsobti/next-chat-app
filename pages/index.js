434a97df20c258398363e12596c9f372fa121fe7
Raghav <raghav_sobti@yahoo.com>
2022-03-17 12:32:43 +0530

    Authentication added
    Adde auth and integrated chat engine api

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,57 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../context";
+import { useRouter } from "next/router";
+import axios from "axios";
 
 export default function Auth() {
-  return <div className="background">auth</div>;
+  const { username, setUsername, secret, setSecret } = useContext(Context);
+
+  const router = useRouter();
+
+  function onSubmit(e) {
+    e.preventDefault();
+    if (username.length === 0 || secret.length === 0) return;
+
+    axios
+      .put(
+        "https://api.chatengine.io/users/",
+        {
+          username,
+          secret,
+        },
+        {
+          headers: { "Private-Key": "05f6f2f3-8e52-495f-a3e5-4876f556ef59" },
+        }
+      )
+      .then((response) => router.push("/chats"));
+  }
+  return (
+    <div className="background">
+      <div className="auth-container">
+        <form className="auth-form" onSubmit={onSubmit}>
+          <div className="auth-title">NextJS Chat</div>
+
+          <div className="input-container">
+            <input
+              placeholder="Email"
+              className="text-input"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+
+          <div className="input-container">
+            <input
+              type="password"
+              placeholder="Password"
+              className="text-input"
+              onChange={(e) => setSecret(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="submit-button">
+            Login/Sign up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
